614a2725d5afdb2e6b1808bfae35586ad75d03d4
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-03-10 14:39:13 -0400

    Created form to submit new message including handling of the post request.

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,19 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Message', messages });
+  res.render('index', { title: 'Message Board', messages });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res, next) => {
+  let newMessage = {text: req.body.message, user: req.body.name, added: new Date()};
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
+
 
 module.exports = router;
