c61d5cd2e3ad5069c0e3b54881a8fd28451bab24
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-03-10 13:25:39 -0400

    Added array of messages and displayed them on index route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {text: "Hi there!", user: "Amanda", added: new Date()},
+  {text: "Howdy!", user: "John", added: new Date()},
+  {text: "Greetings.", user: "George", added: new Date()},
+  {text: "Yo!", user: "Casie", added: new Date()},
+  {text: "Well ain't it a party in here!", user: "Jorge", added: new Date()},
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message', messages });
 });
 
 module.exports = router;
